b6dc32b894a31f20e1970211ead52bcf89a2ab42
Muhammad Cendika Pramono <mcendikap123@gmail.com>
2024-12-16 20:06:28 +0700

    Add files

---
+++ README.md
@@ -0,0 +1,5 @@
+## Portfolio Tailwind CSS Landing Page
+
+<p>
+  Selamat datang di portfolio modern yang dirancang dengan Tailwind CSS. Menampilkan proyek dan keterampilan dengan desain responsif, tipografi bersih, dan komponen interaktif. Ideal untuk memamerkan karya profesional dengan estetika dan fungsi tinggi.
+</p>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        'lg': '850px',
+        'xl': '1100px'
+      }
+    },
+  },
+  
+  plugins: [],
+}
+
